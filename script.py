6e09276ccbd26e66cb8f9d60b9db73f4a35645da
CĂTĂLIN-MIRCEA AFRĂSINEI <catalin.afrasinei@stud.ubbcluj.ro>
2022-07-20 17:14:12 +0300

    working search

--- script.py
+++ script.py
@@ -9,6 +9,7 @@ from selenium.webdriver.common.by import By
 # getting the driver
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
+
 driver = webdriver.Chrome(options=options)
 driver.get("https://youtube.com")
 
@@ -16,16 +17,23 @@ driver.get("https://youtube.com")
 acceptCookiesButton = driver.find_element(By.XPATH, '/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/a/tp-yt-paper-button')
 acceptCookiesButton.click()
 
-time.sleep(2.5)
+# time delay is needed to wait for the page to load
+time.sleep(2)
 
 # searching for the video
 searchBox = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
+SEARCH_TEXT = ""
 
 if len(sys.argv) > 1:
-    searchText = str(sys.argv[1])
-    print(searchText)
-    searchBox.send_keys(searchText)
+    for i in range(1, len(sys.argv)):
+        SEARCH_TEXT += str(sys.argv[1])
+    print(SEARCH_TEXT)
 else:
-    searchBox.send_keys('Cat Videos')
+    SEARCH_TEXT = "Cat Videos"
 
+searchBox.send_keys(SEARCH_TEXT)
 searchBox.submit()
+
+
+
+# driver.close()
