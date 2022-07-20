c84c2bb80b00f09e04f82b8f122dc9d9b147418d
CĂTĂLIN-MIRCEA AFRĂSINEI <catalin.afrasinei@stud.ubbcluj.ro>
2022-07-20 19:38:57 +0300

    working skipping ads ( i hope )

--- script.py
+++ script.py
@@ -5,21 +5,26 @@ import numpy as np
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 
 # getting the driver
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("--start-maximized")
+# options.add_argument("--start-maximized") #to start in fullscreen
 
 driver = webdriver.Chrome(options=options)
 driver.get("https://youtube.com")
 
-# accepting the cookies popup
-acceptCookiesButton = driver.find_element(By.XPATH, '/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/a/tp-yt-paper-button')
-acceptCookiesButton.click()
+# accepting the cookies popup, if one appears
+try:
+    acceptCookiesButton = driver.find_element(By.XPATH, '/html/body/ytd-app/ytd-consent-bump-v2-lightbox/tp-yt-paper-dialog/div[4]/div[2]/div[6]/div[1]/ytd-button-renderer[2]/a/tp-yt-paper-button')
+    acceptCookiesButton.click()
+except NoSuchElementException:
+    pass
 
 # time delay is needed to wait for the page to load
-time.sleep(2)
+time.sleep(1.5)
 
 # searching for the video
 searchBox = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/div/ytd-masthead/div[3]/div[2]/ytd-searchbox/form/div[1]/div[1]/input')
@@ -30,14 +35,40 @@ if len(sys.argv) > 1:
         SEARCH_TEXT += str(sys.argv[i]) + " "
     print(SEARCH_TEXT)
 else:
-    SEARCH_TEXT = "Cat Videos"
+    SEARCH_TEXT = "Funny Videos"
 
 searchBox.send_keys(SEARCH_TEXT)
-searchBox.submit()
+searchBox.send_keys(Keys.ENTER)
 
-ytVideo = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a/yt-img-shadow/img')
-ytVideo.click()
+time.sleep(1)
+
+searchedVideo = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-search/div[1]/ytd-two-column-search-results-renderer/div/ytd-section-list-renderer/div[2]/ytd-item-section-renderer/div[3]/ytd-video-renderer[1]/div[1]/ytd-thumbnail/a/yt-img-shadow/img')
+searchedVideo.click()
+
+time.sleep(0.5)
+
+youtubeVideo = driver.find_element(By.XPATH,'/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div')
 
 
+while 'ad-showing' in youtubeVideo.get_attribute('class').split() and 'paused-mode' not in youtubeVideo.get_attribute('class').split():
+    print('Ad is showing')
+    time.sleep(0.5)
+    try:
+        adDuration = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[1]/span/div').text
+        print('Skippable Ad after ' + adDuration + 's')
+        time.sleep(int(adDuration) + 0.5)
+        skipButton = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[3]/div/div[2]/span/button')
+        skipButton.click()
+        print('Ad Skipped')
+    except NoSuchElementException:
+        adDuration = driver.find_element(By.XPATH, '/html/body/ytd-app/div[1]/ytd-page-manager/ytd-watch-flexy/div[5]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[17]/div/div[2]/span[2]/div').text.split(":")[1]
+        print('Unskippable Ad of ' + adDuration + 's')
+        time.sleep(int(adDuration) + 0.5)
 
-# driver.close()
+
+if 'paused-mode' in youtubeVideo.get_attribute('class').split():
+    print("Manually interrupted")
+
+youtubeVideo.click()
+
+driver.quit()
